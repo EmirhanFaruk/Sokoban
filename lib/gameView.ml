6b203ae514561f634b5e4805773e51152d8d7d64
Le Roy Julie <julieleroy317@gmail.com>
2024-10-29 21:39:08 +0100

    ajout gras

--- lib/gameView.ml
+++ lib/gameView.ml
@@ -3,14 +3,15 @@ open GameState
 (* Convertit une tuile en un caractère *)
 let cell_to_emoji cell = 
   match cell with
-  | Wall -> "\xF0\x9F\x9F\xa8"
+  | Wall -> "\xF0\x9F\x9F\xA8"
   | Ground -> "\xE2\xAC\x9B"
   | Box -> "\xF0\x9F\x93\xA6"
-  | BoxGround ->  "\xF0\x9F\x9F\xA2"
+  | BoxGround ->  "\xF0\x9F\x9F\xA9"
   | Player -> "\xF0\x9F\x9A\xB6"
 
 (* Affichage de la carte par défaut *)
-let showMap (map: tile list list) =
+let printMap (map: tile list list) =
   List.iter (fun row ->
     List.iter (fun cell -> print_string (cell_to_emoji cell)) row;  (* Correctement terminer la fonction interne *)
     print_newline ()) map
+
