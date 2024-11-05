0faedc94b476a20aae44da12012c6c9f4f1a16e6
EmirhanFaruk <emirhansagir74@gmail.com>
2024-11-05 01:04:37 +0100

    Small fix(real)

--- lib/Scoreboard.ml
+++ lib/Scoreboard.ml
@@ -16,10 +16,10 @@ struct
     let chnl = open_in filename in
     let rec loop () =
         try
-            let next = input_line f in
+            let next = input_line chnl in
             next :: loop ()
         with End_of_file ->
-            close_in f;
+            close_in chnl;
             []
         in loop ()
 
@@ -31,12 +31,14 @@ struct
         (fun acc line ->
             let parts = Str.split (Str.regexp ";") line in
             if int_of_string (List.nth parts 1) = level then
-            let new_score = {
-                                name = List.nth parts 0;
-                                level = int_of_string (List.nth parts 1);
-                                moves = int_of_string (List.nth parts 2);
-                            } in
-            new_score :: acc
+                let new_score = {
+                                    name = List.nth parts 0;
+                                    level = int_of_string (List.nth parts 1);
+                                    moves = int_of_string (List.nth parts 2);
+                                } in
+                new_score::acc
+            else
+                acc
         ) [] list in
         let arres = Array.of_list res in
         Array.sort (fun s1 s2 -> compare s1.moves s2.moves) arres;
@@ -47,9 +49,9 @@ struct
 
     (* Gets score data in a list of array of score. *)
     let get_score_data filename =
-        let lines = read_file "./assert/scores.txt" in
+        let lines = read_file filename in
         let score_arr_list = ref [] in
-        for i = 0 to List.length lines do
+        for i = 0 to (List.length lines) - 1 do
         	let score_arr = list_to_score lines i in
         	if Array.length score_arr <> 0 then
         	score_arr_list := score_arr :: !score_arr_list
@@ -66,45 +68,62 @@ struct
         then
             max_name - (String.length score.name)
         else
-            0 in
+            0
+        in
 
 
         let move_rest =
-        if max_move > String.length (string_of_int el.move)
+        if max_move > String.length (string_of_int score.moves)
         then
-            max_move - String.length (string_of_int el.move)
+            max_move - String.length (string_of_int score.moves)
         else
-            0 in
+            0
+        in
 
         let name = score.name ^ (String.make name_rest ' ') in
-        let score = (String.make move_rest ' ') ^ (string_of_int el.move) in
+        let score = (String.make move_rest ' ') ^ (string_of_int score.moves) in
 
         name ^ (String.make extra_padding ' ') ^ score
 
 
+    (* Gets first 10 elements of an array *)
+    let get_10_els arr =
+        let len = Array.length arr in
+        let res = Array.make len (arr.(0)) in
+        for i = 0 to len do
+        	Array.set res i (arr.(i))
+        done;
+        res
+
     (* Gets top 10 of a level and puts them in a string list *)
     let get_level_scoreboard level =
         let all_scores = get_score_data "./assert/scores.txt" in
-        let wanted_scores_full =
-            List.filter (fun s -> s.level = level) all_scores in
+        let wanted_list =
+        List.filter (fun x -> x.(0).level = level) all_scores in
 
         let top_10_scores =
-            List.filteri (fun i _ -> i < 10) wanted_scores_full in
+            Array.to_list (get_10_els (List.hd wanted_list)) in
 
 
         let longest_name_len =
         List.fold_left
         (fun acc el ->
-            if (String.length el.name) > acc then
-            acc = (String.length el.name)
+            if (String.length el.name) > acc
+            then
+                (String.length el.name)
+            else
+                acc
         ) 0 top_10_scores in
 
         let longest_move_len =
         List.fold_left
         (fun acc el ->
-            let len = String.length (string_of_int el.move) in
-            if len > acc then
-            acc = len
+            let len = String.length (string_of_int el.moves) in
+            if len > acc
+            then
+                len
+            else
+                acc
         ) 0 top_10_scores in
 
         let total_len = 41 in
@@ -124,11 +143,11 @@ struct
             fun acc score ->
             let scstr =
             (String.make border ' ') ^
-            (score_to_str score max_name max_move padding) ^
+            (score_to_str score longest_name_len longest_move_len padding) ^
             (String.make border ' ') in
             scstr :: acc
 
-        ) [] top_10_scores in
+        ) [] (Array.of_list top_10_scores) in
         res
 
 
@@ -145,20 +164,20 @@ struct
     (* Prints given level's scoreboard. Level will be printed as if it's +1 *)
     let print_level_scoreboard level =
         let scores = get_level_scoreboard level in
-        let sb_text = "Top 10 of level " ^ (string_of_int (level + 1))
+        let sb_text = "Top 10 of level " ^ (string_of_int (level + 1)) in
 
         print_string "\x1b[1m";
         print_endline (center_text sb_text 41);
 
         let lines_left = 10 - List.length scores in
 
-        let print_scores scores =
+        let rec print_scores scores =
             match scores with
             | [] -> ()
             | s :: xs -> print_endline s; print_scores xs
         in print_scores scores;
 
-        for i = 0 to lines_left do
+        for _ = 0 to lines_left - 1 do
             print_endline "";
         done;
         print_string "\x1b[0m";
@@ -174,26 +193,34 @@ struct
         !res
 
     let scoreboard_menu () =
-        let levels = Array.of_list get_levels in
-        let index = 0 in
+        let levels = Array.of_list (get_levels ()) in
+        let index = ref 0 in
         if Array.length levels = 0
         then
             print_endline "No scores found"
         else
-            while index <> -1 do
+            while !index <> -1 do
                 GameView.clear_terminal ();
-                print_level_scoreboard levels.(index);
+                print_level_scoreboard levels.(!index);
                 print_endline "(Previous: b, Next: n, Quit: x) : ";
                 let choice = read_line () in
-                match choice with
-                | b -> if index = 0
-                        then index = (Array.length levels) - 1
-                        else index = index - 1
-                | n -> if index = (Array.length levels) - 1
-                        then index = 0
-                        else index = index + 1
-                | x -> GameView.clear_terminal (); index = -1
-                | _ -> GameView.clear_terminal ()
+                if String.equal choice "b"
+                then
+                    if !index = 0
+                    then
+                        index := (Array.length levels) - 1
+                    else
+                        index := !index - 1
+                else if String.equal choice "n"
+                then
+                    if !index = (Array.length levels) - 1
+                    then
+                        index := 0
+                    else
+                        index := !index + 1
+                else if String.equal choice "x"
+                then
+                    index := -1
             done
 
 end
