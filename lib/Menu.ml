8c3b4cec5baf4ec941e3a09f659423d23ce85e82
EmirhanFaruk <emirhansagir74@gmail.com>
2024-11-07 09:47:01 +0100

    me literally when the

--- lib/Menu.ml
+++ lib/Menu.ml
@@ -78,7 +78,7 @@ struct
     print_string "\x1b[0m";
     if Sys.os_type = "Unix" then (print_string "Appuyez sur Entrer pour retourner au menu..."; let _ = read_line () in (); clear_terminal ());
     flush stdout;  (* S'assurer que l'affichage est fait *)
-    let _ = read_line () in ()
+    if Sys.os_type <> "Unix" then (let _ = read_line () in ());
   
 
   (*------------------------------------------------------ FONCTION PRINCIPALE ----------------------------------------------------------------*)
