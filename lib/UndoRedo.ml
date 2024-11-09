0a15ea67a2cc1fd1abd975d0d0719e37ce4aa595
Le Roy Julie <julieleroy317@gmail.com>
2024-11-09 22:17:36 +0100

    Ajout de commentaire

--- lib/UndoRedo.ml
+++ lib/UndoRedo.ml
@@ -5,33 +5,43 @@ open Player
 module UndoRedo =
 struct
 
+  (* Type représentant une sauvegarde avec les coordonnées et les tuiles avant et après déplacement *)
   type save = { x : int; y : int; before : Tile.tile; after : Tile.tile }
 
+  (* Type représentant les piles de undo et redo représenté par une liste de liste de sauvegarde*)
   type stacks = {
     mutable undoStack : save list list;
     mutable redoStack : save list list;
   }
 
+  (* Fonction pour initialiser les piles *)
   let initializeStacks () = {undoStack = []; redoStack = []}
 
+  (* Fonction pour reinitialiser les piles *)
   let resetStacks stacks =
     stacks.undoStack <- [];
     stacks.redoStack <- []
-
+  
+  (* Fonction pour créer une sauvegarde *)  
   let makeSave x y before after =
     {x; y; before; after}
-
+  
+  (* Fonction qui ajoute une liste de sauvegarde à la pile undo *)
   let addSavesToStack stacks save_list =
     if List.length save_list > 0 then
     (stacks.undoStack <- save_list :: stacks.undoStack;
     stacks.redoStack <- [])
-
+  
+  (* Fonction qui met à jour la map selon les sauvegardes *)
   let update map player stat (liste : save list) isBefore =
+    (* Pour chaque sauvegarde de la liste on met à jour map *)
     List.iter (fun s -> 
+      (* isBefore est un boolean pour savoir si on utilise la fonction pour undo ou redo*)
       let tile = 
         if isBefore then s.before
         else s.after in
-      GameState.modifyList map s.x s.y tile;
+      GameState.modifyList map s.x s.y tile; (* mise à jour de la map *)
+      (* mise à jour du joueur *)
       if tile = Tile.Player then
         (if isBefore then Player.stat_down stat
         else Player.stat_upt stat;
@@ -40,9 +50,10 @@ struct
 
   (* Fonction pour retourner en arrière *)
   let undo map player (stacks : stacks) stat=
+    (* On met à jour la map avec la premiere liste de sauvegarde de la pile*)
     match stacks.undoStack with
     | h:: t -> update map player stat h true;
-      
+      (* mise à jour des stacks *)
       let new_redo = h :: stacks.redoStack in
       stacks.undoStack <- t;
       stacks.redoStack <- new_redo
