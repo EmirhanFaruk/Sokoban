172a966c3ab9e87c8a5088b27025f893af043602
EmirhanFaruk <emirhansagir74@gmail.com>
2024-11-13 23:03:47 +0100

    removed unused funcs in mli files

--- lib/Menu.mli
+++ lib/Menu.mli
@@ -4,12 +4,6 @@ module Menu : sig
   (* Fonction pour nettoyer le terminal *)
   val clear_terminal : unit -> unit
 
-  (* Affiche le menu principal et retourne le choix de l'utilisateur *)
-  val showMenu : unit -> char
-
-  (* Affiche les règles du jeu *)
-  val showRules : unit -> unit
-
   (* Fonction principale qui affiche le menu et traite le choix du joueur *)
   val mainMenu : unit -> unit
 
--- lib/ScoreBoard.mli
+++ lib/ScoreBoard.mli
@@ -11,21 +11,6 @@ module Scoreboard : sig
   (* Sauvegarder le score dans le fichier *)
   val save_score : Player.stat -> int -> unit
 
-  (* Lire le fichier donné *)
-  val read_file : string -> string list
-
-  (* Transformer une liste de string dans une liste de score en le triant par le niveau donné. Format de fichier: nom;niveau;deplacement *)  
-  val list_to_score : string list -> int -> score array
-
-  (* Avoir la data de score dans une liste d'array de score *)
-  val get_score_data : string -> score array list
-
-  (* Transformer le score en string. Ajout d'espaces autour du texte pour ameliorer affichage. *) 
-  val score_to_str : score -> int -> int -> int -> string
-
-  (* Avoir les 10 premiers elements d'un array *)
-  val get_10_els : 'a array -> 'a array
-
   (* Ajouter dans une liste de string les 10 meilleures performance d'un niveau *)
   val get_level_scoreboard : int -> string list
 
